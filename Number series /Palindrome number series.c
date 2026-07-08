59d06a6ef974bd4a8f313cf9964693064aeefe1e
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-08 06:39:17 +0530

    Create Palindrome number series.c

---
+++ Number series /Palindrome number series.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, temp, rev, rem;
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        temp = i;
+        rev = 0;
+
+        while(temp != 0)
+        {
+            rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+
+        if(rev == i)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
