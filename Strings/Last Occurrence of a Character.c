64e8a75e8bad854c7af157422479b085612a1033
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-12 23:06:56 +0530

    Create Last Occurrence of a Character.c

---
+++ Strings/Last Occurrence of a Character.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i = 0, pos = -1;
+
+    scanf("%s", str);
+    scanf(" %c", &ch);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == ch)
+        {
+            pos = i;
+        }
+        i++;
+    }
+
+    printf("%d", pos);
+
+    return 0;
+}
