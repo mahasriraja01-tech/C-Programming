8300cd70bb3104a004791b23ac8e1121349527fb
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-16 22:18:03 +0530

    Create Caesar Cipher Decryption.c

---
+++ Strings/Caesar Cipher Decryption.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int key, i = 0;
+
+    scanf("%s", str);
+    scanf("%d", &key);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = (str[i] - 'a' - key + 26) % 26 + 'a';
+
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = (str[i] - 'A' - key + 26) % 26 + 'A';
+
+        i++;
+    }
+
+    printf("%s", str);
+
+    return 0;
+}
