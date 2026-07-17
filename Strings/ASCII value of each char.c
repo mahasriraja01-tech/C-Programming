8c930399ef6c25e54f19b865967963f633ebdfe1
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-17 14:05:57 +0530

    Create ASCII value of each char.c

---
+++ Strings/ASCII value of each char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+
+    scanf("%s", str);
+
+    while (str[i] != '\0')
+    {
+        printf("%c = %d\n", str[i], str[i]);
+        i++;
+    }
+
+    return 0;
+}
