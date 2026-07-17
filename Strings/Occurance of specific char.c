c496af293c642abb71334fff3fd151129fe2a302
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-17 14:03:11 +0530

    Create Occurance of specific char.c

---
+++ Strings/Occurance of specific char.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100], ch;
+    int i = 0, count = 0;
+
+    scanf("%s", str);
+    scanf(" %c", &ch);
+
+    while (str[i] != '\0')
+    {
+        if (str[i] == ch)
+            count++;
+
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
