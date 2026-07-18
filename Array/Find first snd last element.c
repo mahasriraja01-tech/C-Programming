66ed65160c7ac9c9321921ac08b7e7caa64bf40a
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-18 19:52:13 +0530

    Create Find first snd last element.c

---
+++ Array/Find first snd last element.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i;
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("First = %d\n", a[0]);
+    printf("Last = %d", a[4]);
+
+    return 0;
+}
