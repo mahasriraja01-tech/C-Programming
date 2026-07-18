255d5bfed4b110463559753942a19690dcadb57e
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-18 19:53:05 +0530

    Create Difference between max and min.c

---
+++ Array/Difference between max and min.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i, max, min, difference;
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    max = a[0];
+    min = a[0];
+
+    for(i = 1; i < 5; i++)
+    {
+        if(a[i] > max)
+        {
+            max = a[i];
+        }
+
+        if(a[i] < min)
+        {
+            min = a[i];
+        }
+    }
+
+    difference = max - min;
+
+    printf("%d", difference);
+
+    return 0;
+}
