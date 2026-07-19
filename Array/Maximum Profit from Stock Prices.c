918c4a7d6dd97bc37fb665145baca443e0e5000a
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-19 22:13:02 +0530

    Create Maximum Profit from Stock Prices.c

---
+++ Array/Maximum Profit from Stock Prices.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100];
+    int n,i,min,maxProfit=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    min=a[0];
+
+    for(i=1;i<n;i++)
+    {
+        if(a[i]-min>maxProfit)
+            maxProfit=a[i]-min;
+
+        if(a[i]<min)
+            min=a[i];
+    }
+
+    printf("%d",maxProfit);
+
+    return 0;
+}
