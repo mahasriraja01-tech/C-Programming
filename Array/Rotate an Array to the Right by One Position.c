a24970a8277c54ebcc6e2e802d42a085c0804481
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-19 21:49:51 +0530

    Create Rotate an Array to the Right by One Position.c

---
+++ Array/Rotate an Array to the Right by One Position.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i, temp;
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &a[i]);
+
+    temp = a[4];
+
+    for(i = 4; i > 0; i--)
+        a[i] = a[i - 1];
+
+    a[0] = temp;
+
+    for(i = 0; i < 5; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
