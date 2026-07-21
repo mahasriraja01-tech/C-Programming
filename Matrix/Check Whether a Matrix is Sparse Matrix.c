9bf30731e7d1d2b869f8675e01d6ad087b4bacab
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-21 23:19:09 +0530

    Create Check Whether a Matrix is Sparse Matrix.c

---
+++ Matrix/Check Whether a Matrix is Sparse Matrix.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r,c,i,j,zero=0;
+
+    scanf("%d %d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
+            if(a[i][j]==0)
+                zero++;
+        }
+
+    if(zero>(r*c)/2)
+        printf("Sparse Matrix");
+    else
+        printf("Not Sparse Matrix");
+
+    return 0;
+}
