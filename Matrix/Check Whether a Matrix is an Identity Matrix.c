2b602cf4d4fcdd9e4f1967994d68bdfac7f78620
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-21 23:17:05 +0530

    Create Check Whether a Matrix is an Identity Matrix.c

---
+++ Matrix/Check Whether a Matrix is an Identity Matrix.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, flag=1;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            if((i==j && a[i][j]!=1) || (i!=j && a[i][j]!=0))
+                flag=0;
+
+    if(flag)
+        printf("Identity Matrix");
+    else
+        printf("Not Identity Matrix");
+
+    return 0;
+}
