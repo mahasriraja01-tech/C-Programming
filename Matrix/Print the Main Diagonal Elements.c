7ad42b81f0f4ecbd1f1411d3a465983b4b524fda
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-21 23:10:59 +0530

    Create Print the Main Diagonal Elements.c

---
+++ Matrix/Print the Main Diagonal Elements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        printf("%d ",a[i][i]);
+
+    return 0;
+}
