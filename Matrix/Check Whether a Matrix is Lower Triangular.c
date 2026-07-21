1fcf2a9453f15ddbfacb6ac3c7a12958c45142e8
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-21 23:18:36 +0530

    Create Check Whether a Matrix is Lower Triangular.c

---
+++ Matrix/Check Whether a Matrix is Lower Triangular.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], n, i, j, flag=1;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+
+    for(i=0;i<n;i++)
+        for(j=i+1;j<n;j++)
+            if(a[i][j]!=0)
+                flag=0;
+
+    if(flag)
+        printf("Lower Triangular");
+    else
+        printf("Not Lower Triangular");
+
+    return 0;
+}
