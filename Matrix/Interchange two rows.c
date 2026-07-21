12f616e54cfaa21785f3ef47ef0ab77416217394
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-21 23:20:18 +0530

    Create Interchange two rows.c

---
+++ Matrix/Interchange two rows.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r,c,i,j,r1,r2,temp;
+
+    scanf("%d %d",&r,&c);
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    scanf("%d %d",&r1,&r2);
+
+    for(j=0;j<c;j++){
+        temp=a[r1][j];
+        a[r1][j]=a[r2][j];
+        a[r2][j]=temp;
+    }
+
+    for(i=0;i<r;i++){
+        for(j=0;j<c;j++)
+            printf("%d ",a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
