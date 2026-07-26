15b0f35c1c9affb6b65dc527b53623a92a4ea435
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-26 23:07:50 +0530

    Create Print Alternating 1 and 0.c

---
+++ Patterns/Print Alternating 1 and 0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        if(i % 2 == 1)
+            printf("1 ");
+        else
+            printf("0 ");
+    }
+
+    return 0;
+}
