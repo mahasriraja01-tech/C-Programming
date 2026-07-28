af1f77871d845d8d166c34a050179a82080a8e1a
Maha Sri M R <mahasriraja01@gmail.com>
2026-07-28 23:00:29 +0530

    Create Reverse Character Triangle.c

---
+++ Patterns/Reverse Character Triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j;
+    char ch='A';
+
+    scanf("%d",&n);
+
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
